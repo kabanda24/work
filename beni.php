2ad78bdf36e4511d151ca0f2b9040325e6573acf
kabanda24 <127331084+kabanda24@users.noreply.github.com>
2023-05-09 09:34:50 +0200

    Add files via upload

---
+++ beni.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="admin.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>online education</title>
+</head>
+<body>
+    <div class="naav">
+        <nav>
+            
+            <ul>
+                
+                <li><a href="beni.html">Home</a></li>
+                <li><a href="login.html">Trending Books</a></li>
+                <li><a href="login.html">Accademic</a></li>
+                <li><a href="login.html">Uploaded</a></li>
+                <li><a href="login.html">About Us</a></li>
+                <li><a href="login.html">Contact Us</a></li>
+               <div class="btn">
+               <button type="submit"><a href="">LOG IN</a></button>
+
+            </div>
+            </ul>
+          
+        </nav>
+    </div>
+      <div class="main">
+        <h3>Online Education</h3>
+      </div>
+      <div class="memo">
+       <p> You are most wellcome To this Website!</p>
+       <p> Where You can study online based on nothing You </p>
+       <p>Here you can get new annowncement
+        </p><p> and trending news in education</p>
+    </div>
+    <div class="maxm">
+        <p> You are most wellcome To this Website!</p>
+        <p> Where You can study online based on nothing You </p>
+        <p>Here you can get new annowncement
+         </p><p> and trending news in education</p>
+     </div>
+     <div class="vevo">
+        <h1>ONLINE EDUCATION</h1>
+        <h4>Can You Continue With Us </h4>
+     </div> 
+     <div class="img">
+    <img src="download.jpeg" alt="">
+</div>
+<div class="footer">
+<h4>About us</h4>
+<p>Owner: Lycee De KICUKIRO APADE</p> 
+<p>developer: Group 2</p> 
+  
+</div>
+</body>
+</html>
